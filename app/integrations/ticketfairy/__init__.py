ede5d0461beadaad1ca20efd8fa81f1747f732c4
Greg Ipp <gregipp@gmail.com>
2025-07-28 15:27:24 -0700

    Removed unnecessary debug

--- app/integrations/ticketfairy/__init__.py
+++ app/integrations/ticketfairy/__init__.py
@@ -1,8 +1,1 @@
 """TicketFairy integration"""
-
-import logging
-import traceback
-
-logging.basicConfig(level=logging.INFO)
-logging.info("TICKETFAIRY __init__.py loaded")
-logging.info("".join(traceback.format_stack()))
