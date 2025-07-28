a7753dc144aad1341e0234b3e9724f497a44b69b
Greg Ipp <gregipp@gmail.com>
2025-07-28 15:06:05 -0700

    Fix bad init

--- app/integrations/ticketfairy/__init__.py
+++ app/integrations/ticketfairy/__init__.py
@@ -1,6 +1,1 @@
-"""TicketFairy integration"""
 
-from app.integrations.ticketfairy import mcp_tools
-from app.integrations.ticketfairy.base import TicketFairyIntegration
-
-TicketFairyIntegration.mcp_tools = mcp_tools
